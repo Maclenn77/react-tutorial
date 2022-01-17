cfc8d84419b0e3a904dcad76e2416ffcd4b69e81
Maclenn77 <maclenn77@gmail.com>
2022-01-17 14:05:58 -0600

    Setup linters

--- react-todo-app/src/components/TodoItem.js
+++ react-todo-app/src/components/TodoItem.js
@@ -1,13 +1,15 @@
-import React from "react"
+import React from 'react';
 
 class TodoItem extends React.Component {
   render() {
     return (
-        <li>
-          <input type="checkbox" /> {this.props.todo.title}  
-          </li>
-      )
-    }
+      <li>
+        <input type="checkbox" />
+        {' '}
+        {this.props.todo.title}
+      </li>
+    );
+  }
 }
 
-export default TodoItem
+export default TodoItem;
--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TodoContainer from "./components/TodoContainer";
+import TodoContainer from './components/TodoContainer';
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+ReactDOM.render(<TodoContainer />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
